e45e8046112f018daff7b98f27c802f41d5c0fe9
esteban0829 <esteban0829@naver.com>
2021-02-13 20:52:19 +0900

    VI-5 fix build file version issue

--- src/main/kotlin/com/microservice/chapter3/service/Game/GameServiceImpl.kt
+++ src/main/kotlin/com/microservice/chapter3/service/Game/GameServiceImpl.kt
@@ -10,25 +10,33 @@ import com.microservice.chapter3.service.Customer.Customer
 //        var playerSelection = gamePlayer.selection
 //        var differ = playerSelection - computerSelection
 //
-//        if((computerSelection == 2 && playerSelection == 0 ) || (computerSelection == 0 && playerSelection == 2)){
+//        if(differ == 0){ // draw
 //
-//        }else if(differ > 0){
+//        }else if(differ > 0){ // win
 //
-//        }else if(differ < 0)
+//        }else if(differ < 0){ // lose
+//
+//        }
 //    }
 //}
 
 /*
-주먹 가위
-- 주먹 주먹
-주먹 보
 
-- 가위 가위
-가위 주먹
-가위 보
+0 : Rock
+1 : Scissor
+2 : Paper
 
-보 가위
-보 주먹
-- 보 보
+       P C
+주먹 가위 0 1 -1
+주먹 주먹 0 0 0
+주먹 보  0 2 -2
+
+가위 가위 1 1
+가위 주먹 1 0
+가위 보  1 2
+
+보 가위 2 1
+보 주먹 2 0
+보 보
 
  */
